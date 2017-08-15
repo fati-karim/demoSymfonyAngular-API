dbb0df7c6d20ef646496b40a60ae51352a928172
fkarimoullah <fkarimoullah@norsys.fr>
2017-08-15 16:08:33 +0100

    configuring redirection api and web

--- src/MembreShipBundle/Controller/MembreApiController.php
+++ src/MembreShipBundle/Controller/MembreApiController.php
@@ -3,9 +3,12 @@
 namespace MembreShipBundle\Controller;
 
 use MembreShipBundle\Entity\Membre;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Monolog\Logger;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,9 +20,11 @@ class MembreApiController extends Controller
      * @Route("/api/membres", name="liste_membres-api")
      * @Method("GET")
      */
-    public function indexApiAction(Request $request)
+    public function indexAction(Request $request)
     {
         $membresDoctrine = $this->getDoctrine()->getRepository('MembreShipBundle:Membre')->findAll();
+
+        $this->get('logger')->log(Logger::EMERGENCY,' HHHHHHHHHHHHHH detailed debug output');
 
         $membres =  array();
         $i=0;
@@ -37,6 +42,30 @@ class MembreApiController extends Controller
         }
 
         return new JsonResponse($membres);
-        /*return new Response("membres");*/
+    }
+
+    /**
+     * @Route("/api/membres/ajouter", name="create_membre-api")
+     * @Method("POST")
+     */
+    public function createAction(Request $request)
+    {
+
+        $this->get('logger')->log(Logger::EMERGENCY,' OOOOOOOOO detailed debug output');
+
+        $membreApi = json_decode($request->request->get('data'));
+       
+
+        $membre->setName($membreApi->name);
+        $membre->setProfil($membreApi->profil);
+        $membre->setAge($membreApi->age);
+        $membre->setStars($membreApi->stars);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($membre);
+        $em->flush();
+
+        return indexAction($request);
+
     }
 }
