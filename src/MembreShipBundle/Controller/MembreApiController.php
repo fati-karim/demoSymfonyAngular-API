96be73c3d9989ec8f315b0d2776a178eecdec83b
fkarimoullah <fkarimoullah@norsys.fr>
2017-08-28 11:59:28 +0100

    update un membre

--- src/MembreShipBundle/Controller/MembreApiController.php
+++ src/MembreShipBundle/Controller/MembreApiController.php
@@ -45,6 +45,26 @@ class MembreApiController extends Controller
     }
 
     /**
+     * @Route("/api/membres/{id}", name="detail_membre-api")
+     * @Method("GET")
+     */
+    public function detailAction($id)
+    {
+        
+        $membresDoctrine = $this->getDoctrine()->getRepository('MembreShipBundle:Membre')->find($id);
+
+        $membre = array(
+            'id' => $membresDoctrine->getId() , 
+            'name' => $membresDoctrine->getName() , 
+            'profil' => $membresDoctrine->getProfil(), 
+            'age' => $membresDoctrine->getAge() , 
+            'stars' => $membresDoctrine->getStars() 
+        );
+        
+        return new JsonResponse($membre);
+    }
+
+    /**
      * @Route("/api/membres/add", name="create_membre-api")
      * @Method("POST")
      */
@@ -56,6 +76,7 @@ class MembreApiController extends Controller
         $parametersAsArray = $request->request->get('membre');
         
         $membre = new Membre;
+        $membre->setName($parametersAsArray['id']);
         $membre->setName($parametersAsArray['name']);
         $membre->setProfil($parametersAsArray['profil']);
         $membre->setAge($parametersAsArray['age']);
@@ -78,19 +99,23 @@ class MembreApiController extends Controller
               
         $parametersAsArray = [];
         $parametersAsArray = $request->request->get('membre');
-        
-        $membre = new Membre;
-        $membre->setName($parametersAsArray['id']);
-        $membre->setName($parametersAsArray['name']);
-        $membre->setProfil($parametersAsArray['profil']);
-        $membre->setAge($parametersAsArray['age']);
-        $membre->setStars($parametersAsArray['stars']);
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($membre);
+        
+        $membresDoctrine = $em->getRepository('MembreShipBundle:Membre')->find($parametersAsArray['id']);
+
+        if (!$membresDoctrine) {
+            throw $this->createNotFoundException('No membre found for id ='.$productId);
+        }
+
+        $membresDoctrine->setName($parametersAsArray['name']);
+        $membresDoctrine->setProfil($parametersAsArray['profil']);
+        $membresDoctrine->setAge($parametersAsArray['age']);
+        $membresDoctrine->setStars($parametersAsArray['stars']);
+
         $em->flush();
 
-        return new JsonResponse($membre);
+        return new JsonResponse($membresDoctrine);
 
     }
 }
